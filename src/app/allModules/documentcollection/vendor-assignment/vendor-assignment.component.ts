3fce0a11cb6e60c9c00c826818adb28c86d5c473
Ramanji <80185@EXALCA.CORP>
2019-07-23 14:16:21 +0530

    RamanjiChanges18-07

---
+++ src/app/allModules/documentcollection/vendor-assignment/vendor-assignment.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { FuseConfigService } from '@fuse/services/config.service';
+import { config } from 'rxjs';
+import { MatTableDataSource } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
+
+@Component({
+  selector: 'app-vendor-assignment',
+  templateUrl: './vendor-assignment.component.html',
+  styleUrls: ['./vendor-assignment.component.scss']
+})
+export class VendorAssignmentComponent implements OnInit {
+  SupplierInviteClassList: SupplierInviteClass[] = [];
+  BGClassName: any;
+  displayedColumns: string[] = ['select', 'VendorName', 'GSTNumber', 'VendorType', 'City'];
+  dataSource: MatTableDataSource<SupplierInviteClass>;
+  selection = new SelectionModel<SupplierInviteClass>(true, []);
+  constructor(private _fuseConfigService: FuseConfigService) { }
+
+  ngOnInit() {
+    this.SupplierInviteClassList = [
+      {
+        VendorName: 'Exalca Technologies Pvt Ltd', GSTNumber: '29AEDF80091', VendorType: '', City: 'Bangalore , KA ,India',
+      },
+      {
+        VendorName: 'Inowits Technologies Pvt Ltd', GSTNumber: '29AEDF80092', VendorType: '', City: 'Bangalore , KA ,India',
+      },
+      {
+        VendorName: 'Exalca Technologies Pvt Ltd', GSTNumber: '29AEDF80091', VendorType: '', City: 'Bangalore , KA ,India',
+      },
+      {
+        VendorName: 'Inowits Technologies Pvt Ltd', GSTNumber: '29AEDF80092', VendorType: '', City: 'Bangalore , KA ,India',
+      },
+      {
+        VendorName: 'Exalca Technologies Pvt Ltd', GSTNumber: '29AEDF80091', VendorType: '', City: 'Bangalore , KA ,India',
+      },
+      {
+        VendorName: 'Inowits Technologies Pvt Ltd', GSTNumber: '29AEDF80092', VendorType: '', City: 'Bangalore , KA ,India',
+      },
+    ];
+    this.dataSource = new MatTableDataSource(this.SupplierInviteClassList);
+    this.isAllSelected();
+    this.masterToggle();
+    this.checkboxLabel();
+    this._fuseConfigService.config.subscribe((config) => {
+      this.BGClassName = config;
+    })
+  }
+  isAllSelected(): boolean {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle(): void {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: SupplierInviteClass): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.GSTNumber + 1}`;
+  }
+}
+export class SupplierInviteClass {
+  VendorName: string;
+  GSTNumber: string;
+  VendorType: string;
+  City: string;
+}
