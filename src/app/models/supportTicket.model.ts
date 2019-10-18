ff0236f36d17e8a69c32393133f0d173f165584a
exalcams <exalca.plant1@gmail.com>
2019-10-18 18:35:49 +0530

    Support Ticket started

---
+++ src/app/models/supportTicket.model.ts
@@ -0,0 +1,109 @@
+export class STCategory {
+    ID: number;
+    Category: string;
+    IsActive: boolean;
+    CreatedOn: Date;
+    CreatedBy: string;
+    ModifiedOn?: Date;
+    ModifiedBy: string;
+}
+export class STReson {
+    ID: number;
+    Reson: string;
+    IsActive: boolean;
+    CreatedOn: Date;
+    CreatedBy: string;
+    ModifiedOn?: Date;
+    ModifiedBy: string;
+}
+export class STExpectedResult {
+    ID: number;
+    ExpectedResult: string;
+    IsActive: boolean;
+    CreatedOn: Date;
+    CreatedBy: string;
+    ModifiedOn?: Date;
+    ModifiedBy: string;
+}
+export class SupportTicket {
+    TicketID: number;
+    Category: string;
+    ReferenceNumber: string;
+    Reason: string;
+    ExpectedResult: string;
+    Query: string;
+    SupportTicketStatus: string;
+    IsActive: boolean;
+    CreatedOn: Date;
+    CreatedBy: string;
+    ModifiedOn?: Date;
+    ModifiedBy: string;
+}
+
+export class SupportTicketUserMap {
+    ID: number;
+    TicketID: number;
+    Email: string;
+    IsActive: boolean;
+    CreatedOn: Date;
+    CreatedBy: string;
+    ModifiedOn?: Date;
+    ModifiedBy: string;
+}
+export class SupportTicketResponse {
+    ID: number;
+    TicketID: number;
+    Comments: string;
+    IsActive: boolean;
+    CreatedOn: Date;
+    CreatedBy: string;
+    ModifiedOn?: Date;
+    ModifiedBy: string;
+}
+export class SupportTicketWithEmails {
+    TicketID: number;
+    Category: string;
+    ReferenceNumber: string;
+    Reason: string;
+    ExpectedResult: string;
+    Query: string;
+    SupportTicketStatus: string;
+    EmailAddresses: string[];
+    CreatedOn: Date;
+    CreatedBy: string;
+    CreatedByUser: string;
+    ModifiedOn?: Date;
+    ModifiedBy: string;
+    ModifiedByUser: string;
+}
+export class SupportTicketResponseView {
+    ID: number;
+    TicketID: number;
+    Comments: string;
+    IsActive: boolean;
+    CreatedOn: Date;
+    CreatedBy: string;
+    CreatedByUser: string;
+}
+export class SupportTicketStatusView {
+    TicketID: number;
+    SupportTicketStatus: string;
+    Reason: string;
+    ModifiedBy: string;
+}
+export class SupportTicketConfirmationStatusView {
+    Actiontype: string;
+    Reason: string;
+}
+export class STCategoryView {
+    ID: number;
+    Category: string;
+}
+export class STReasonView {
+    ID: number;
+    Reson: string;
+}
+export class STExpectedResultView {
+    ID: number;
+    ExpectedResult: string;
+}
---
+++ src/app/services/support-ticket.service.ts
@@ -0,0 +1,102 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { HttpErrorResponse, HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { STCategoryView, STExpectedResultView, STReasonView, SupportTicketWithEmails, 
+  SupportTicketResponseView, SupportTicketResponse, SupportTicketStatusView } from 'app/models/supportTicket.model';
+import { Auxiliary } from 'app/models/asn';
+import { Guid } from 'guid-typescript';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupportTicketService {
+
+  baseAddress: string;
+  constructor(private _httpClient: HttpClient, private _authService: AuthService) {
+    this.baseAddress = _authService.baseAddress;
+  }
+
+  // Error Handler
+  errorHandler(error: HttpErrorResponse): Observable<string> {
+    return throwError(error.error || error.message || 'Server Error');
+  }
+
+  GetAllCategories(): Observable<STCategoryView[] | string> {
+    return this._httpClient.get<STCategoryView[]>(`${this.baseAddress}api/SupportTicket/GetAllCategories`)
+      .pipe(catchError(this.errorHandler));
+  }
+  GetAllReasons(): Observable<STReasonView[] | string> {
+    return this._httpClient.get<STReasonView[]>(`${this.baseAddress}api/SupportTicket/GetAllReasons`)
+      .pipe(catchError(this.errorHandler));
+  }
+  GetAllExpectedResults(): Observable<STExpectedResultView[] | string> {
+    return this._httpClient.get<STExpectedResultView[]>(`${this.baseAddress}api/SupportTicket/GetAllExpectedResults`)
+      .pipe(catchError(this.errorHandler));
+  }
+  CreateSupportTicket(supportTicketWithEmails: SupportTicketWithEmails): Observable<any> {
+    return this._httpClient.post<any>(`${this.baseAddress}api/SupportTicket/CreateSupportTicket`,
+      supportTicketWithEmails,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      })
+      .pipe(catchError(this.errorHandler));
+  }
+
+  AddSupportTicketAttachment(auxiliary: Auxiliary, selectedFiles: File[]): Observable<any> {
+    const formData: FormData = new FormData();
+    if (selectedFiles && selectedFiles.length) {
+      selectedFiles.forEach(x => {
+        formData.append(x.name, x, x.name);
+      });
+    }
+    formData.append('APPID', auxiliary.APPID.toString());
+    formData.append('APPNumber', auxiliary.APPNumber.toString());
+    formData.append('CreatedBy', auxiliary.CreatedBy);
+    return this._httpClient.post<any>(`${this.baseAddress}api/SupportTicket/AddSupportTicketAttachment`,
+      formData,
+      // {
+      //   headers: new HttpHeaders({
+      //     'Content-Type': 'application/json'
+      //   })
+      // }
+    ).pipe(catchError(this.errorHandler));
+
+  }
+
+  GetSupportTicketByVendor(UserID: Guid): Observable<SupportTicketWithEmails[] | string> {
+    return this._httpClient.get<SupportTicketWithEmails[]>(`${this.baseAddress}api/SupportTicket/GetSupportTicketByVendor?UserID=${UserID}`)
+      .pipe(catchError(this.errorHandler));
+  }
+  GetSupportTicketByBuyer(UserID: Guid): Observable<SupportTicketWithEmails[] | string> {
+    return this._httpClient.get<SupportTicketWithEmails[]>(`${this.baseAddress}api/SupportTicket/GetSupportTicketByBuyer?UserID=${UserID}`)
+      .pipe(catchError(this.errorHandler));
+  }
+  CreateSupportTicketResponse(supportTicketResponse: SupportTicketResponse): Observable<any> {
+    return this._httpClient.post<any>(`${this.baseAddress}api/SupportTicket/CreateSupportTicketResponse`,
+      supportTicketResponse,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      })
+      .pipe(catchError(this.errorHandler));
+  }
+  GetSupportTicketResponseByTicketID(TicketID: number): Observable<SupportTicketResponseView[] | string> {
+    return this._httpClient.get<SupportTicketResponseView[]>(`${this.baseAddress}api/SupportTicket/GetSupportTicketResponseByTicketID?TicketID=${TicketID}`)
+      .pipe(catchError(this.errorHandler));
+  }
+  UpdateSupportTicketStatus(StatusView: SupportTicketStatusView): Observable<any> {
+    return this._httpClient.post<any>(`${this.baseAddress}api/SupportTicket/SupportTicketStatusView`,
+    StatusView,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      })
+      .pipe(catchError(this.errorHandler));
+  }
+}
